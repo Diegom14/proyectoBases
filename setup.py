b3f5796116fccdeecb853f5ee50482149d4c301f
yerkortiz <yerko.ortizm@mail.udp.cl>
2017-10-22 00:09:32 -0300

    Update setup.py
    A la tabla choques se le agregó fecha para así poder estudio de choques de un intervalo de tiempo, de donde derivan las siguientes posibles consultas: 
    -Promedio de choques al mes 
    -Porcentaje de choques anuales en los que estuvo involucrado un conductor ebrio 
    -Rut y alcoholemia de conductores que han estado en  3 o mas choques en un intervalo de 6 meses
    A la tabla choques se le agregó fallecidos de donde derivan las siguientes consultas:
    -Cantidad de fallecidos anualmente
    -Porcentaje de choques donde hubo uno o mas fallecidos y uno o mas conductores iban en estado de ebriedad
    -Rut y alcoholemia de conductores que estuvieron involucrados en choques donde hubieron fallecidos
    A vehiculo se le agrego dueño_id:
    -Cantidad de choques donde el conductor no era el dueño del vehiculo
    A personas se le agrego edad:
    -Porcentaje de choques donde el conductor era menor de edad e iba en estado de ebriedad
    Otras posibles consultas a tomar en cuenta:
    -modelo y marca del vehiculo que estuvo involucrado en mas choques
    -patente del vehiculo y cantidad de choques en el que estuvo involucrado

--- setup.py
+++ setup.py
@@ -10,16 +10,15 @@ cur.execute(sql)
 
 sql = """
 CREATE TABLE personas
-			(id serial PRIMARY KEY, rut int(8),  dv varchar(1), cantidad_choques int(10));	
-
+			(id serial PRIMARY KEY, rut int(8),  dv varchar(1), cantidad_choques int(10), edad int(10));	
 
 CREATE TABLE vehiculo
-			(id serial PRIMARY KEY, patente varchar(40),  personas_id int(100), modelo varchar(40), marcas varchar(20));	
+			(id serial PRIMARY KEY, patente varchar(40),  personas_id int(100), modelo varchar(40), marca varchar(20), dueño_id int(10));	
 
 CREATE TABLE conductor_choque
 			(personas_id int(10), vehiculo_id int(10), choque_id int(10), testimonio text, alcoholemia int(10));
 
 CREATE TABLE choques
-			(id serial PRIMARY KEY, vehiculo_id int(10), personas_id int(10));
+			(id serial PRIMARY KEY, vehiculo_id int(10), personas_id int(10), fallecidos int(100), fecha varchar(10));
 """
 cur.execute(sql)
