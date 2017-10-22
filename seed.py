507d40671d48474eca9a87e4450978d63e730354
Diegom14 <diego.munozv@mail.udp.cl>
2017-10-22 19:54:44 -0300

    insert listo

--- seed.py
+++ seed.py
@@ -3,17 +3,27 @@ import psycopg2
 conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
 cur = conn.cursor()
 
-sql ="""
-INSERT INTO personas (rut, dv, cantidad_choques, edad) values (165455, "?", 1, 27)
+sql =""" 
+INSERT INTO personas (rut, dv, cantidad_choques, edad, nombre, direccion) values (165455, '1', 1, 27, 'JUAN SOTO', 'ejercito 4');
+ 
+INSERT INTO personas (rut, dv, cantidad_choques, edad, nombre, direccion) values (195421, '2', 1, 23, 'ROBERTO DIAZ', 'ejercito 5');
+
+INSERT INTO personas (rut, dv, cantidad_choques, edad, nombre, direccion) values (128554, '3', 1, 39, 'JUANA DE ARCO', 'ejercito 2');
+ 
+INSERT INTO personas (rut, dv, cantidad_choques, edad, nombre, direccion) values (130544, '4', 1, 32, 'MARIA MONTT', 'ejercito 1');
+
+INSERT INTO vehiculo (patente, personas_id, modelo, marca) values ('hdf21', 2, '48 GTB', 'FERRARI' );
+
+INSERT INTO vehiculo (patente, personas_id, modelo, marca) values ('KJf21', 1, 'MORNING', 'KYA' );
+
+INSERT INTO vehiculo (patente, personas_id, modelo, marca) values ('hlo31', 3, 'SWIFT', 'SUZUKI' );
+
+INSERT INTO choques (vehiculo_id, personas_id, cantidad_fallecidos, fecha) values (2, 1, 0, '11-5-2011');
+
+INSERT INTO conductor_choque (personas_id, vehiculo_id, choque_id, testimonio, alcoholemia) values (1, 2, 1, 'conductor ebrio', 1.1);
+
+
  """
-sql ="""
-INSERT INTO personas (rut, dv, cantidad_choques, edad) values (195421, "?", 1, 23)
- """
-sql ="""
-INSERT INTO personas (rut, dv, cantidad_choques, edad) values (128554, "?", 1, 39)
- """
-sql ="""
-INSERT INTO personas (rut, dv, cantidad_choques, edad) values (130544, "?", 1, 32)
- """
-sql ="""
-"""
+cur.execute(sql)
+conn.commit()
+cur.close()
--- setup.py
+++ setup.py
@@ -3,22 +3,23 @@ import psycopg2
 conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
 
 cur = conn.cursor()
-sql = """DROP SCHEMA public CASCADE;
-CREATE SHEMA public;"""
 
-cur.execute(sql)
 
 sql = """
 CREATE TABLE personas
-			(id serial PRIMARY KEY, rut int(8),  dv varchar(1), cantidad_choques int(10), edad int(10), nombre varchar(10), apellido varchar(10), direccion varchar(40));	
+			(id serial PRIMARY KEY, rut INT,  dv varchar(1), cantidad_choques int, 
+			edad int, nombre varchar(40), direccion varchar(40));	
 
 CREATE TABLE vehiculo
-			(id serial PRIMARY KEY, patente varchar(40),  personas_id int(100), modelo varchar(40), marca varchar(20), dueño_id int(10));	
+			(id serial PRIMARY KEY, patente varchar(40),  personas_id int, 
+			modelo varchar(40), marca varchar(20));	
 
 CREATE TABLE conductor_choque
-			(personas_id int(10), vehiculo_id int(10), choque_id int(10), testimonio text, alcoholemia int(10));
+			(personas_id int, vehiculo_id int, choque_id int, alcoholemia numeric(11, 10), testimonio text);
 
 CREATE TABLE choques
-			(id serial PRIMARY KEY, vehiculo_id int(10), personas_id int(10), fallecidos int(100), fecha varchar(10));
+			(id serial PRIMARY KEY, vehiculo_id int, personas_id int, cantidad_fallecidos int, fecha varchar(10));
 """
 cur.execute(sql)
+conn.commit()
+cur.close()
