518e5fa5d310ce82401fe9173b5c53286ce7a370
yerkortiz <yerko.ortizm@mail.udp.cl>
2017-10-22 00:24:03 -0300

    Update seed.py
    Comenta para que es dv y a que se refiere cantidad de choques (¿choques en los que han estado involucrados a lo largo de su vida?) en la tabla personas, estaba pensando en agregarles el nombre y dirección para hacer mas consultas

--- seed.py
+++ seed.py
@@ -4,3 +4,16 @@ conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
 cur = conn.cursor()
 
 sql ="""
+INSERT INTO personas (rut, dv, cantidad_choques, edad) values (165455, "?", 1, 27)
+ """
+sql ="""
+INSERT INTO personas (rut, dv, cantidad_choques, edad) values (195421, "?", 1, 23)
+ """
+sql ="""
+INSERT INTO personas (rut, dv, cantidad_choques, edad) values (128554, "?", 1, 39)
+ """
+sql ="""
+INSERT INTO personas (rut, dv, cantidad_choques, edad) values (130544, "?", 1, 32)
+ """
+sql ="""
+"""
