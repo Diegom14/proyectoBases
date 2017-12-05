04048b735af1ec827b395e800c2a63941cd3cbe3
Diegom14 <diego.munozv@mail.udp.cl>
2017-12-05 13:31:55 -0300

    Actualizado

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ cur = conn.cursor()
 
 sql = """
 CREATE TABLE personas
-			(id serial PRIMARY KEY, rut INT,  dv varchar(1), cantidad_choques int, 
-			edad int, nombre varchar(50), direccion varchar(50), comuna varchar(50));	
+			(id serial PRIMARY KEY, rut INT,  dv int, cantidad_choques int, 
+			edad int, nombre varchar(50), calle varchar(50), numeracion int, comuna varchar(50));	
 
 CREATE TABLE vehiculo
 			(id serial PRIMARY KEY, patente varchar(50),  personas_id int, 
 			marca varchar(50), modelo varchar(50));	
 
 CREATE TABLE conductor_choque
-			(personas_id int, vehiculo_id int, choque_id int, alcoholemia numeric(11, 10));
+			(personas_id int, vehiculo_id int, choque_id int, alcoholemia numeric(11, 10), PRIMARY KEY(personas_id, vehiculo_id, choque_id));
 
 CREATE TABLE choques
 			(id serial PRIMARY KEY, vehiculo_id int, personas_id int, fecha timestamp, comuna varchar(50));
